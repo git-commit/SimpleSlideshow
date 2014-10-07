82448b898b357208480130c5b8741a8184683585
Maximilian Berger <snowdragon92@gmail.com>
2014-10-07 11:10:03 +0200

    Initial version.

---
+++ Slideshow.py
@@ -0,0 +1,71 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+from os import listdir
+from os.path import isfile, join
+import os
+import sys
+import random
+import time
+import threading
+
+
+class Slideshow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(Slideshow, self).__init__()
+        self.scene = QtWidgets.QGraphicsScene()
+        self.graphics_view = QtWidgets.QGraphicsView(self.scene)
+        self.files = Slideshow.get_all_pictures(os.getcwd())
+        self.setCentralWidget(self.graphics_view)
+        self.setWindowTitle('Slideshow')
+        #self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setWindowState(QtCore.Qt.WindowFullScreen)
+        self.show()
+
+        self._stopThread = False
+        self.thread = threading.Thread(target=slideTime, args=(self, 60))
+        self.thread.daemon = True
+        self.thread.start()
+
+    def nextPicture(self):
+        r = random.randint(0, len(self.files) - 1)
+        self._change_picture(self.files[r])
+
+    def _change_picture(self, file):
+        pixmap = QtGui.QPixmap(os.path.join(os.getcwd(), file))
+        item = QtWidgets.QGraphicsPixmapItem(pixmap)
+        self.scene.clear()
+        self.scene.addItem(item)
+        print("Change picture: " + str(file))
+        self.graphics_view.fitInView(item, QtCore.Qt.KeepAspectRatio)
+
+    def stopThread(self):
+        return self._stopThread
+
+    @staticmethod
+    def get_all_pictures(path):
+        picture_types = ('.jpg', '.png', '.JPG', '.PNG')
+        return [f for f in listdir(path) if isfile(join(path, f))
+                and os.path.splitext(f)[1] in picture_types]
+
+    def closeEvent(self, event):
+        print("Setting thread variable to false.")
+        self._stopThread = True
+        event.accept()  # let the window close
+
+
+def slideTime(slide, delay):
+    print("Starting thread")
+
+    while not slide.stopThread():
+        slide.nextPicture()
+        time.sleep(delay)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    s = Slideshow()
+    i = app.exec_()
+    sys.exit(i)
+
+if __name__ == '__main__':
+    main()
---
+++ setup-freeze.py
@@ -0,0 +1,75 @@
+import sys
+from cx_Freeze import setup, Executable
+
+
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = "Console"
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables = [
+    Executable(
+        'Slideshow.py',
+        base=base,
+        )
+]
+
+# http://msdn.microsoft.com/en-us/library/windows/desktop/aa371847(v=vs.85).aspx
+shortcut_table = [
+    ("DesktopShortcut",            # Shortcut
+     "DesktopFolder",              # Directory_
+     "Slideshow",                  # Name
+     "TARGETDIR",                  # Component_
+     "[TARGETDIR]Slideshow.exe",   # Target
+     None,                         # Arguments
+     None,                         # Description
+     None,                         # Hotkey
+     None,                         # Icon
+     None,                         # IconIndex
+     None,                         # ShowCmd
+     'TARGETDIR'                   # WkDir
+     ),
+    ("StartMenuShortcut",            # Shortcut
+     "StartMenuFolder",              # Directory_
+     "Slideshow",                  # Name
+     "TARGETDIR",                  # Component_
+     "[TARGETDIR]Slideshow.exe",   # Target
+     None,                         # Arguments
+     None,                         # Description
+     None,                         # Hotkey
+     None,                         # Icon
+     None,                         # IconIndex
+     None,                         # ShowCmd
+     'TARGETDIR'                   # WkDir
+     )
+
+    ]
+
+# Now create the table dictionary
+msi_data = {"Shortcut": shortcut_table}
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+copyfiles = [
+    ('C:\Python34\Lib\site-packages\PyQt5\libEGL.dll', 'libEGL.dll'),
+    ]
+build_exe_options = dict(packages=[], excludes=[], include_files=copyfiles)
+
+bdist_msi_options = {
+    "upgrade_code": "{183d99bc-6632-4235-8afc-c952da8639a3}",
+    'data': msi_data
+}
+
+
+setup(name='Slideshow',
+      version='0.2.0',
+      description='Simple slideshow utility that shows all pictures in the current working directory',
+      url='http://xerael.net',
+      author='Maximilian Berger',
+
+      options=dict(build_exe=build_exe_options,
+                   bdist_msi=bdist_msi_options),
+      executables=executables
+      )
